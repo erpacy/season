718103cc4a04cdeccc6e78f3e7b6c98345169cb5
AcyYu <997954474@qq.com>
2016-11-21 17:18:47 +0800

    精简mybatis和事务配置

--- src/main/java/org/seasonteam/SeasonApplication.java
+++ src/main/java/org/seasonteam/SeasonApplication.java
@@ -1,46 +1,16 @@
 package org.seasonteam;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("org.seasonteam.season.mapper")
+@EnableTransactionManagement
 public class SeasonApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeasonApplication.class, args);
     }
-
-    // DataSource配置
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return new org.apache.tomcat.jdbc.pool.DataSource();
-    }
-
-    // 提供SqlSeesion
-    @Bean
-    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource());
-        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-
-        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mapper/*.xml"));
-        sqlSessionFactoryBean.setTypeAliasesPackage("org.seasonteam.season.model");
-
-        return sqlSessionFactoryBean.getObject();
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        return new DataSourceTransactionManager(dataSource());
-    }
+    
 }
